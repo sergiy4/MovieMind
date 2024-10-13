88a33b347efddcdb07e8bb38b199ad9eb5b252bb
Sergiy <sergiy0378@gmail.com>
2024-10-13 17:36:39 +0300

    MM-0: + vite

---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+    return <h1>APP</h1>;
+};
+
+export { App };
---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'node:url';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: '~',
+                replacement: fileURLToPath(new URL('src', import.meta.url)),
+            },
+        ],
+    },
+});
