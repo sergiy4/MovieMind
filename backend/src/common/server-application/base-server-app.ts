aad11fe9623bc1f115b980e196bcf677311b7590
Sergiy <sergiy0378@gmail.com>
2024-10-14 18:20:40 +0300

    MM-00: + logger

--- backend/src/common/server-application/base-server-app.ts
+++ backend/src/common/server-application/base-server-app.ts
@@ -1,18 +1,37 @@
 import Fastify, { type FastifyInstance } from 'fastify';
 
+import { type Logger } from '../logger/logger.js';
 import { type ServerApp } from './types/types.js';
+
+type Constructor = {
+    logger: Logger;
+};
 
 class BaseServerApp implements ServerApp {
     private app: FastifyInstance;
 
-    public constructor() {
+    private logger: Logger;
+
+    public constructor({ logger }: Constructor) {
+        this.logger = logger;
         this.app = Fastify();
     }
 
     public async init(): Promise<void> {
-        await this.app.listen({
-            port: 3000,
-        });
+        this.logger.info('Application initialization…');
+
+        await this.app
+            .listen({
+                port: 3000,
+            })
+            .catch((error: Error) => {
+                this.logger.error(error.message, {
+                    cause: error.cause,
+                    stack: error.stack,
+                });
+            });
+
+        this.logger.info('Application is listening on PORT – 3000');
     }
 }
 
--- backend/src/common/server-application/server-application.ts
+++ backend/src/common/server-application/server-application.ts
@@ -1,5 +1,6 @@
+import { logger } from '../logger/logger.js';
 import { BaseServerApp } from './base-server-app.js';
 
-const serverApp = new BaseServerApp();
+const serverApp = new BaseServerApp({ logger });
 
 export { serverApp };
