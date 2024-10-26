562e503284e5657ea4607a6e911646e16baf3891
Serhii Diachok <sergiy0378@gmail.com>
2024-10-26 20:03:43 +0300

    MM-00: + config type

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,2 +1,4 @@
-export { type HttpMethod,HTTPMethod } from './framework/http/http.js';
+export { AppEnvironment } from './enums/enums.js';
+export { type Config } from './framework/config/config.js';
+export { type HttpMethod, HTTPMethod } from './framework/http/http.js';
 export { type ValueOf } from './types/types.js';
