1d6b400c3f05581a0581f76f2f17ed00382670ea
Serhii Diachok <sergiy0378@gmail.com>
2024-11-07 13:24:04 +0200

    MM-02: + server app

--- backend/src/common/server-application/base-server-app.ts
+++ backend/src/common/server-application/base-server-app.ts
@@ -1,10 +1,27 @@
-import Fastify, { type FastifyInstance } from 'fastify';
+import Fastify, {
+    type FastifyError,
+    type FastifyInstance,
+    type FastifyReply,
+    type FastifyRequest,
+} from 'fastify';
 
+import { HttpError } from '~/bundles/auth/types/types.js';
+
+import { type Database } from '../database/database.js';
+import { HttpCode } from '../enums/enums.js';
 import { type Logger } from '../logger/logger.js';
-import { type ServerApp } from './types/types.js';
+import { ServerErrorType } from './enums/enums.js';
+import {
+    type ServerApp,
+    type ServerAppApi,
+    type ServerAppRouteParameters,
+    type ServerCommonErrorResponse,
+} from './types/types.js';
 
 type Constructor = {
     logger: Logger;
+    database: Database;
+    apis: ServerAppApi[];
 };
 
 class BaseServerApp implements ServerApp {
@@ -12,13 +29,85 @@ class BaseServerApp implements ServerApp {
 
     private logger: Logger;
 
-    public constructor({ logger }: Constructor) {
+    private database: Database;
+
+    private apis: ServerAppApi[];
+
+    public constructor({ logger, apis, database }: Constructor) {
         this.logger = logger;
+        this.database = database;
+        this.apis = apis;
         this.app = Fastify();
+    }
+
+    public addRoute(parameters: ServerAppRouteParameters): void {
+        const { path, method, handler } = parameters;
+
+        this.app.route({
+            url: path,
+            method,
+            handler,
+        });
+
+        this.logger.info(`Route: ${method as string} ${path} is registered`);
+    }
+
+    public addRoutes(parameters: ServerAppRouteParameters[]): void {
+        for (const it of parameters) {
+            this.addRoute(it);
+        }
+    }
+
+    public initRoutes(): void {
+        const routers = this.apis.flatMap((it) => it.routes);
+
+        this.addRoutes(routers);
+    }
+
+    private initErrorHandler(): void {
+        this.app.setErrorHandler(
+            (
+                error: FastifyError,
+                _request: FastifyRequest,
+                reply: FastifyReply,
+            ) => {
+                if (error instanceof HttpError) {
+                    this.logger.error(
+                        `[Http Error]: ${error.status.toString()} – ${
+                            error.message
+                        }`,
+                    );
+
+                    const response: ServerCommonErrorResponse = {
+                        errorType: ServerErrorType.COMMON,
+                        message: error.message,
+                    };
+
+                    return reply.status(error.status).send(response);
+                }
+
+                this.logger.error(error.message);
+
+                const response: ServerCommonErrorResponse = {
+                    errorType: ServerErrorType.COMMON,
+                    message: error.message,
+                };
+
+                return reply
+                    .status(HttpCode.INTERNAL_SERVER_ERROR)
+                    .send(response);
+            },
+        );
     }
 
     public async init(): Promise<void> {
         this.logger.info('Application initialization…');
+
+        this.initErrorHandler();
+
+        this.initRoutes();
+
+        this.database.connect();
 
         await this.app
             .listen({
--- backend/src/common/server-application/server-application.ts
+++ backend/src/common/server-application/server-application.ts
@@ -1,6 +1,12 @@
+import { authController } from '~/bundles/auth/auth.js';
+
+import { database } from '../database/database.js';
 import { logger } from '../logger/logger.js';
 import { BaseServerApp } from './base-server-app.js';
+import { BaseServerAppApi } from './base-server-app-api.js';
 
-const serverApp = new BaseServerApp({ logger });
+const apiV1 = new BaseServerAppApi('v1', ...authController.routes);
+
+const serverApp = new BaseServerApp({ logger, apis: [apiV1], database });
 
 export { serverApp };
