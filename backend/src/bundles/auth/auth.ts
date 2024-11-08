2da8598df231b838110b870e114589f61c6fb1ca
Serhii Diachok <sergiy0378@gmail.com>
2024-11-08 15:41:57 +0200

    MM-03: + sign in functionality

--- backend/src/bundles/auth/auth.ts
+++ backend/src/bundles/auth/auth.ts
@@ -1,11 +1,15 @@
 import { logger } from '~/common/logger/logger.js';
-import { tokenService } from '~/common/services/services.js';
+import { cryptService, tokenService } from '~/common/services/services.js';
 
 import { userService } from '../users/users.js';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 
-const authService = new AuthService({ tokenService, userService });
+const authService = new AuthService({
+    tokenService,
+    userService,
+    cryptService,
+});
 const authController = new AuthController(logger, authService);
 
-export { authController,authService };
+export { authController, authService };
