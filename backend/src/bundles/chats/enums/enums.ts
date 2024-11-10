f9f05a73b6f136c16e8fe92c3ce74100cd69bd7e
Serhii Diachok <sergiy0378@gmail.com>
2024-11-10 20:33:21 +0200

    MM-12: + exports

---
+++ backend/src/bundles/chats/enums/enums.ts
@@ -0,0 +1,2 @@
+export { ChatErrorMessage } from './chat-error-message.enum.js';
+export { ApiPath, ChatApiPath } from 'shared';
---
+++ backend/src/bundles/chats/types/types.ts
@@ -0,0 +1,11 @@
+export {
+    type Chat,
+    type CreateCharResponseDto,
+    type CreateChatRequestDto,
+    type DeleteChatRequestDto,
+    type DeleteChatResponseDto,
+    type GetCurrentChatRequestDto,
+    type GetUserChatListResponseDto,
+    type UpdateChatRequestDto,
+    type UpdateChatResponseDto,
+} from 'shared';
