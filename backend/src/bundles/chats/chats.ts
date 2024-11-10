ff8bd99cdfe32af588565553fb3d290734ac9b97
Serhii Diachok <sergiy0378@gmail.com>
2024-11-10 20:39:25 +0200

    MM-12: + controller

---
+++ backend/src/bundles/chats/chats.ts
@@ -0,0 +1,22 @@
+import { ChatController } from './chat.controller.js';
+import { ChatModel } from './chat.model.js';
+import { ChatRepository } from './chat.repository.js';
+import { ChatService } from './chat.service.js';
+
+const chatRepository = new ChatRepository(ChatModel);
+const chatService = new ChatService({ chatRepository });
+const chatController = new ChatController({ chatService });
+
+export { chatController,chatService };
+export { ChatApiPath, ChatErrorMessage } from './enums/enums.js';
+export {
+    type Chat,
+    type CreateCharResponseDto,
+    type CreateChatRequestDto,
+    type DeleteChatRequestDto,
+    type DeleteChatResponseDto,
+    type GetCurrentChatRequestDto,
+    type GetUserChatListResponseDto,
+    type UpdateChatRequestDto,
+    type UpdateChatResponseDto,
+} from './types/types.js';
