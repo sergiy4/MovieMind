89a7c61056a9468f5ebea608060f7d0171e3890c
Serhii Diachok <sergiy0378@gmail.com>
2024-11-16 18:51:58 +0200

    MM-22: * enum

--- shared/src/bundles/messages/enums/message-validation-message.enum.ts
+++ shared/src/bundles/messages/enums/message-validation-message.enum.ts
@@ -1,6 +1,8 @@
 const MessageValidationMessage = {
     CONTENT_REQUIRE: 'Message is required',
     CONTENT_LENGTH: 'Message must have from 1 to 1000 characters',
+    OFFSET_IS_REQUIRED: 'Offset is required',
+    LIMIT_IS_REQUIRED: 'Limit is required',
 } as const;
 
 export { MessageValidationMessage };
