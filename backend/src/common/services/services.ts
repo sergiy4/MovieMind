70c88285d72f3adb35481e36582381c952d2e38a
Serhii Diachok <sergiy0378@gmail.com>
2024-11-18 18:44:04 +0200

    MM-14: + aiAssistant service

--- backend/src/common/services/services.ts
+++ backend/src/common/services/services.ts
@@ -1,4 +1,5 @@
 import { config } from '../config/config.js';
+import { AiAssistant } from './ai-assistant/ai-assistant.service.js';
 import { CryptService } from './crypt/crypt.service.js';
 import { TokenService } from './token/token.service.js';
 
@@ -8,5 +9,8 @@ const secretKey = config.ENV.TOKEN.SECRET_KEY;
 const expirationTime = config.ENV.TOKEN.EXPIRATION_TIME;
 const tokenService = new TokenService(secretKey, expirationTime);
 
-export { cryptService,  tokenService };export { CryptService } from './crypt/crypt.service.js';
+const aiAssistant = new AiAssistant(config);
+
+export { aiAssistant,cryptService, tokenService };
+export { CryptService } from './crypt/crypt.service.js';
 export { TokenService } from './token/token.service.js';
