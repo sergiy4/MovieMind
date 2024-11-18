4d330ee117216a3ca71d286b62afbca1196f1766
Serhii Diachok <sergiy0378@gmail.com>
2024-11-18 12:43:19 +0200

    MM-22: * entity and model

--- backend/src/bundles/messages/message.entity.ts
+++ backend/src/bundles/messages/message.entity.ts
@@ -1,5 +1,7 @@
 import { type Entity } from '~/common/types/types.js';
 
+import { MovieEntity } from '../movies/movie.entity.js';
+import { type Movie } from '../movies/movies.js';
 import { type MessageSender } from './types/types.js';
 
 class MessageEntity implements Entity {
@@ -11,21 +13,26 @@ class MessageEntity implements Entity {
 
     private 'chatId': number;
 
+    private 'movies': MovieEntity[];
+
     private constructor({
         id,
         sender,
         content,
         chatId,
+        movies = [],
     }: {
         id: number | null;
         sender: MessageSender;
         content: string;
         chatId: number;
+        movies: MovieEntity[];
     }) {
         this.id = id;
         this.sender = sender;
         this.content = content;
         this.chatId = chatId;
+        this.movies = movies;
     }
 
     public static initialize({
@@ -33,17 +40,20 @@ class MessageEntity implements Entity {
         sender,
         content,
         chatId,
+        movies,
     }: {
         id: number | null;
         sender: MessageSender;
         content: string;
         chatId: number;
+        movies: MovieEntity[];
     }): MessageEntity {
         return new MessageEntity({
             id,
             sender,
             content,
             chatId,
+            movies,
         });
     }
 
@@ -51,16 +61,21 @@ class MessageEntity implements Entity {
         sender,
         content,
         chatId,
+        movies,
     }: {
         sender: MessageSender;
         content: string;
         chatId: number;
+        movies: Movie[];
     }): MessageEntity {
         return new MessageEntity({
             id: null,
             sender,
             content,
             chatId,
+            movies: movies.map((movie) => {
+                return MovieEntity.initializeNew(movie);
+            }),
         });
     }
 
@@ -69,12 +84,16 @@ class MessageEntity implements Entity {
         sender: MessageSender;
         content: string;
         chatId: number;
+        movies: Movie[];
     } {
         return {
             id: this.id as number,
             sender: this.sender,
             content: this.content,
             chatId: this.chatId,
+            movies: this.movies.map((movie) => {
+                return movie.toObject();
+            }),
         };
     }
 
