4fb2799a215a51411e3c5f25afa8aafab6a083c3
Serhii Diachok <sergiy0378@gmail.com>
2024-11-25 17:40:21 +0200

    MM-18: * http

--- backend/src/common/http/http.ts
+++ backend/src/common/http/http.ts
@@ -5,4 +5,5 @@ const baseHttp = new BaseHttp();
 export { baseHttp };
 export { BaseHttpApi } from './base-http-api.js';
 export { HttpError } from './exceptions/exceptions.js';
+export { createAuthorizationHeaderString } from './helpers/helpers.js';
 export { type Http, type HttpMethod } from './types/types.js';
